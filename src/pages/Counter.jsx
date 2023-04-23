1b1e7bbdc0f8c0fc20cafb6aa371e151ac5393ee
Aidana777 <aidananiazalieva27@gmail.com>
2023-04-23 09:59:30 +0600

    Done!!!

---
+++ src/pages/Counter.jsx
@@ -0,0 +1,26 @@
+import { useDispatch, useSelector } from 'react-redux'
+
+
+export const Counter = () => {
+  const dispatch = useDispatch()
+  const  count  = useSelector(state => state)
+
+  const Increment = () => {
+    dispatch({
+      type: 'Increment'
+    })
+  }
+  const Decrement = () => {
+    dispatch({
+      type: 'Decrement'
+    })
+  }
+  return (
+
+    <div>
+      <button onClick={Increment}>Increment</button>
+      <p>{count}</p>
+      <button onClick={Decrement}>Decrement</button>
+    </div>
+  )
+}
---
+++ src/redux/reducer.jsx
@@ -0,0 +1,16 @@
+
+
+export default function reducer(state = 0, action) {
+   switch (action.type) {
+      case "Increment":
+         return state + 1;
+      case "Decrement":
+         return state - 1
+      default:
+         return state
+
+   }
+
+}
+
+
